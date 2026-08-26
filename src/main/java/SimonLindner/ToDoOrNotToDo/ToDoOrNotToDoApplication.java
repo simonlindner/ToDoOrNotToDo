d4dcfbbe39b48281d9e53a11785718c7a9d44274
Simon Lindner <simonlindner26@googlemail.com>
2026-08-26 13:23:55 +0200

    first commit

---
+++ src/main/java/SimonLindner/ToDoOrNotToDo/ToDoOrNotToDoApplication.java
@@ -0,0 +1,13 @@
+package SimonLindner.ToDoOrNotToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoOrNotToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoOrNotToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SimonLindner/ToDoOrNotToDo/ToDoOrNotToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package SimonLindner.ToDoOrNotToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoOrNotToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
